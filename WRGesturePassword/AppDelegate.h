c4e13098769d605649739e77d482eca2ee77f063
王然 <wangran354101066@163.com>
2016-06-24 15:32:02 +0800

    Initial Commit

---
+++ WRGesturePassword/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WRGesturePassword
+//
+//  Created by 王然 on 16/6/24.
+//  Copyright © 2016年 wangran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
